f7b2e248f97ad336891d4474a1583779e800a658
rhidja <rhidja@gmail.com>
2018-07-02 00:50:21 +0200

    Modification du script d'importation

--- install.sh
+++ install.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 MY_PATH="`dirname \"$0\"`"
 
-read -p "Do you want to re-install database (yes or no)? choosing 'yes' will delete all data :" -n 5 confirm
+# ==================================================================================================
+SERVER_NAME="lexik.hidja.loc"
+APP_DIR="html\/lexik"
+APACHE_LOG_DIR="lexik"
+SITES_AVAILABLE="/etc/apache2/sites-available"
+VirtualHost="lexik.hidja.loc.conf"
+# ==================================================================================================
+
+echo "Installation de l'application Shopping Cart sur un serveur apache2"
+echo "==================================================================\n"
+
+read -p "L'execution de ce script, réinitialisera la base de données. Voulez-vous le confirmer? :" -n 5 confirm
 
 if [[ $confirm = "yes" ]]; then
 
@@ -28,8 +39,24 @@ if [[ $confirm = "yes" ]]; then
     npm install @symfony/webpack-encore --save-dev
     yarn run encore dev
 
-    # Run the server
-    php -S 127.0.0.1:8000 -t public &
+    # Configuration du VirtualHost
+    mkdir -p /var/log/apache2/lexik/
+    # Copier dans dossier /etc/apache2/sites-available le fichier de configuration du VirtualHost
+    cp docker/virtualhost.conf.dist $SITES_AVAILABLE/$VirtualHost
+    # Le server name et son alias
+    sed -i -e "s/domain\.tld/$SERVER_NAME/g" $SITES_AVAILABLE/$VirtualHost
+    # Les fichiers de log de l'application
+    sed -i -e "s/project_error/$APACHE_LOG_DIR\/error/g" $SITES_AVAILABLE/$VirtualHost
+    sed -i -e "s/project_access/$APACHE_LOG_DIR\/access/g" $SITES_AVAILABLE/$VirtualHost
+    # Le dossier de l'application
+    sed -i -e "s/project/$APP_DIR/g" $SITES_AVAILABLE/$VirtualHost
+
+    cd $SITES_AVAILABLE/
+    a2ensite $VirtualHost
+    service apache2 restart
+
+    sed -i -e "s/127.0.0.1     $SERVER_NAME//g" /etc/hosts
+    echo "127.0.0.1     $SERVER_NAME" >> /etc/hosts
 
 elif [[ $confirm = "" ]]; then
     echo "Running command canceled"
